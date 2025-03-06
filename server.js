9015c51c85310cf3409858bd1acd23c1955b8e04
Abu Bakr Ahmed <95282969+abubakr-29@users.noreply.github.com>
2025-03-07 01:35:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ const db = new pg.Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 db.connect();
 
